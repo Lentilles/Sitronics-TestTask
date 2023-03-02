f288f734e2a411c859f0db5fd7dc19a037fe38f1
Alexey <Marveleks@gmail.com>
2023-03-02 20:43:33 +0300

    Initial commit

---
+++ Sitronics/ImageReader.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Program;
+
+namespace Sitronics
+{
+    public class ImageReader
+    {
+        public static Coordinates GetCenterOfInput(Bitmap bitmap)
+        {
+            Color PixelColor;
+
+            List<Coordinates> coords = new List<Coordinates>();
+
+            for (int y = 0; y < bitmap.Height; y++)
+            {
+                for (int x = 0; x < bitmap.Width; x++)
+                {
+                    PixelColor = bitmap.GetPixel(x, y);
+                    if (PixelColor == Color.FromArgb(255, 255, 255, 255))
+                    {
+                        coords.Add(new Coordinates(x, y));
+                    }
+                }
+            }
+            if(coords.Count > 0) 
+            {
+                return coords[coords.Count / 2];
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ Sitronics/Program.cs
@@ -0,0 +1,26 @@
+﻿using Sitronics;
+using System.Drawing;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var paths = Directory.GetFiles("..\\..\\..\\Content");
+        var result = "";
+
+        Coordinates coords;
+
+        foreach (var path in paths)
+        {
+            Bitmap bitmap = new Bitmap(path);
+            coords = ImageReader.GetCenterOfInput(bitmap);
+            if(coords != null)
+            {
+                result += $"{Path.GetFileNameWithoutExtension(path)},{coords.x},{coords.y};";
+            }
+        }
+
+        Console.WriteLine(result);
+
+    }
+}
